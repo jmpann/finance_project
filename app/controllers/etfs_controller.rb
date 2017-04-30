2a3b1ea379b9b45475ae735170fe300e57e8b4a4
Josh Pann <josh.pann@example.com>
2017-04-30 19:19:40 -0400

    bug fix

--- app/controllers/etfs_controller.rb
+++ app/controllers/etfs_controller.rb
@@ -18,11 +18,6 @@ class EtfsController < ApplicationController
     @top_holdings = @etf.top_holdings
     @country_weights = @etf.country_weights
     @sector_allocations = @etf.sector_allocations
-
-    respond_to do |format|
-      format.html
-      format.csv { send_data , filename: ".csv" }
-    end
   end
 
 end
